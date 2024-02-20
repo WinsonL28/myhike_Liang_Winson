302ff91957578bcd7feef6f32677a9e677ccb4fe
WinsonL28 <winsonliang@live.com>
2024-02-20 10:54:34 -0800

    completed demo

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyCVqC9AKcgzNXE_m0YWdtPAtE5lBQC4T9k",
+    authDomain: "comp1800-2024100-demo.firebaseapp.com",
+    projectId: "comp1800-2024100-demo",
+    storageBucket: "comp1800-2024100-demo.appspot.com",
+    messagingSenderId: "297661348097",
+    appId: "1:297661348097:web:794026daa0abd3b4278300"
 };
 
 //--------------------------------------------
